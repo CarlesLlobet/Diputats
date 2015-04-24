4dee0536bec6337d96559661340351c2ef3df241
CarlesLlobet <krow.boarder.9@gmail.com>
2015-04-24 12:56:59 +0200

    Grafics 3D al main

--- src/prop/g12/diputats/Main.java
+++ src/prop/g12/diputats/Main.java
@@ -7,6 +7,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot3D;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.util.Rotation;
+
 
 public class Main {
 	
@@ -48,6 +59,41 @@ public class Main {
 		ConjuntDades.getInstance().setCongres(congres);
 	}
 	
+	public static class Ventana extends JFrame{
+	    JPanel panel;
+	    public Ventana(){
+	        setTitle("Como Hacer Graficos con Java");
+	        setSize(800,600);
+	        setLocationRelativeTo(null);
+	        setDefaultCloseOperation(EXIT_ON_CLOSE);
+	        setVisible(true);
+	        init();
+	    }
+	 
+	    private void init() {
+	        panel = new JPanel();
+	        getContentPane().add(panel);
+	        // Fuente de Datos
+	        DefaultPieDataset defaultpiedataset = new DefaultPieDataset();
+	        defaultpiedataset.setValue("Programacion", new Double(41.200000000000003D));
+	        defaultpiedataset.setValue("Electronica", new Double(11D));
+	        defaultpiedataset.setValue("Hacking", new Double(19.5D));
+	        defaultpiedataset.setValue("SEO", new Double(30.5D));
+	        defaultpiedataset.setValue("Redes", new Double(2.0D));
+	 
+	        // Creando el Grafico
+	        JFreeChart chart = ChartFactory.createPieChart3D("Tematicas Blog", defaultpiedataset, true, true, false);
+	        PiePlot3D pieplot3d = (PiePlot3D)chart.getPlot();
+	        pieplot3d.setDepthFactor(0.2);
+	        pieplot3d.setStartAngle(290D);
+	        pieplot3d.setDirection(Rotation.CLOCKWISE);
+	        pieplot3d.setForegroundAlpha(0.5F);
+	        
+	        // Mostrar Grafico
+	        ChartPanel chartPanel = new ChartPanel(chart);
+	        panel.add(chartPanel);
+	    }
+	}
 	
 	
 	public static void main(String[] args) {
@@ -56,6 +102,7 @@ public class Main {
 		//afegirEmpreses();
 		//afegirVots();
 		out.close();
+		
+		new Ventana().setVisible(true);
 	}
-	
 }
