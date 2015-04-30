0bc99c0689f786dc47f56f367f505e52edb4ff30
CarlesLlobet <krow.boarder.9@gmail.com>
2015-04-30 11:29:02 +0200

    ordenacio driverempresa

--- src/prop/g12/diputats/DriverEmpresa.java
+++ src/prop/g12/diputats/DriverEmpresa.java
@@ -8,8 +8,10 @@ public class DriverEmpresa {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introdueix 0 per provar el constructor de la classe Empresa\n"
-				+ "Introdueix 1 per provar getNom\nIntrodueix 2 per provar getAmbit\n"
-				+ "Introdueix 3 per provar afegirDiputat\nIntrodueix 4 per provar getDiputats\n"
+				+ "Introdueix 1 per provar getNom\n "
+				+ "Introdueix 2 per provar getAmbit\n"
+				+ "Introdueix 3 per provar afegirDiputat\n"
+				+ "Introdueix 4 per provar getDiputats\n"
 				+ "Introdueix -1 per a finalitzar l'execucio\n");
 		int opt = sc.nextInt();
 		sc.nextLine();
