d28a181cbd7d5b58f8aad67aba6702b443d6c5b0
CarlesLlobet <krow.boarder.9@gmail.com>
2015-04-30 19:42:25 +0200

    StubGraf

--- src/prop/g12/common/StubGraf.java
+++ src/prop/g12/common/StubGraf.java
@@ -1,9 +1,36 @@
 package prop.g12.common;
 
-public class StubGraf {
+import java.util.ArrayList;
+
+public class StubGraf<T> {
+	
+	Integer[][] adjacencia;
+	Integer numNodes;
+	ArrayList<T> nodes;
+	int c;
 
 	public StubGraf() {
-		// TODO Auto-generated constructor stub
+		numNodes = 3;
+		adjacencia = new Integer[3][3];
+		adjacencia = {1 , -1, 0, 0, 1, -1, 0, 1, -1};
+		nodes = new ArrayList<T> (9);
+		c = 9;
+	}
+	
+	public int GetNumNodes() {
+		return numNodes; 
+	}
+	
+	public T GetNode(Integer idNode) {
+		return nodes.get(idNode);
+	}
+	
+	public ArrayList<T> GetNodes() {
+		return nodes;
+	}
+	
+	public Integer GetAresta(int idNode1, int idNode2) {
+		return adjacencia[idNode1][idNode2];
 	}
 
 }
